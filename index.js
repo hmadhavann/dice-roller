50096d6669d6e2e2ab85d65471d3fe0600d7f1c8
Hima Madhavan <hmadhavan@Himas-MacBook-Air.local>
2024-02-10 15:23:50 -0600

    updated image and final implementations

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
+window.addEventListener("load", function() {
+    rollDice();
+});
 
 function rollDice() {
-    //const diceImage = document.getElementById("diceImage");
-    //const numbers = [];
-   // const image = [];
     const number = Math.floor(Math.random() * 6) + 1;
-    console.log(number);
-   // numbers.push(number);
-    var imagePath = "dice_image/dice" + number + ".png"
-    //image.push(`<img src="dice_images/dice + {number}.png">`)
-    //console.log(numbers);
-    var diceImageDiv = document.getElementById("diceImage");
-    diceImageDiv.innerHTML = `<img src="` + imagePath + `"alt-"Dice ` + number + `">`;
+    var diceImage= "dice_images/dice" + number + ".png"
+    document.querySelector("img").setAttribute("src", diceImage);
+    
 
 }
+//const button = document.querySelector("button");
+
+//button.addEventListener("keyup",(e) => {
+    
+//if (e.keyCode === 13){
+        //rollDice();
+   // }
+//});
+
+
 
